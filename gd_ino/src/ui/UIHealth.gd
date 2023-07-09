e1236f26601df9b28f19ca71d75657393a134a84
syun <2dgames.jp@gmail.com>
2023-07-09 09:44:36 +0900

    ライフの点滅を実装

--- gd_ino/src/ui/UIHealth.gd
+++ gd_ino/src/ui/UIHealth.gd
@@ -1,18 +1,40 @@
 extends Control
 
+# ======================================
+# HP表示UI
+# ======================================
+
+# --------------------------------------
+# const.
+# --------------------------------------
 # ひとまず最大10まで.
 const MAX_HP = 10
 
+# --------------------------------------
+# onready.
+# --------------------------------------
 @onready var _spr = $Health00
 
+# --------------------------------------
+# var.
+# --------------------------------------
 var _spr_list:Array[Sprite2D] = []
 var _hp = 0
 var _max_hp = 0
+var _cnt = 0 # フレームカウンタ.
 
+# --------------------------------------
+# public functions.
+# --------------------------------------
+## HPを設定.
 func set_hp(hp:int, max_hp:int) -> void:
 	_hp = hp
 	_max_hp = max_hp
 
+# --------------------------------------
+# private functions.
+# --------------------------------------
+## 開始.
 func _ready() -> void:
 	for i in range(MAX_HP):
 		var spr = Sprite2D.new()
@@ -25,9 +47,18 @@ func _ready() -> void:
 		add_child(spr)
 	_spr.visible = false # 使用しない.
 
+## 更新.
 func _physics_process(_delta: float) -> void:
+	_cnt += 1
+	var is_blink = (_hp <= 1) # HP1以下で点滅する.
+	
 	for i in range(MAX_HP):
 		var spr = _spr_list[i]
+		
+		if is_blink and _cnt%10 < 5:
+			spr.visible = false
+			continue
+		
 		if i >= _max_hp:
 			# 表示不要.
 			spr.visible = false
