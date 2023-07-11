1e99354b6cc9327e8b544f04012ca50488dc4bc9
Syunsuke Ozeki <2dgames.jp@gmail.com>
2023-07-11 10:48:31 +0900

    リザルト画面をいったん実装

--- gd_ino/src/scenes/result/Result.gd
+++ gd_ino/src/scenes/result/Result.gd
@@ -1,7 +1,48 @@
 extends Node2D
+# =========================================
+# リザルト画面.
+# =========================================
+# -----------------------------------------
+# const.
+# -----------------------------------------
+enum eState {
+	INIT,
+	MAIN,
+	END,
+}
 
+# -----------------------------------------
+# onready.
+# -----------------------------------------
+@onready var _txt_item = $GainItem
+@onready var _txt_time = $ClearTime
+
+# -----------------------------------------
+# var.
+# -----------------------------------------
+var _state = eState.INIT
 var _gained_item = 19
-var _clear_time = (59*1000*60) + (59 * 1000) + 999
+var _clear_time = (59*60) + (59) + 0.999
 
+# -----------------------------------------
+# functions.
+# -----------------------------------------
+## 開始.
 func _ready() -> void:
-	pass
+	_txt_item.text = "%d"%_gained_item
+	var msec = int(_clear_time*1000) % 1000
+	var sec = int(_clear_time) % 60
+	var minute = int(_clear_time/60)
+	var time = "%2d:%02d.%03d"%[minute, sec, msec]
+	_txt_time.text = time
+
+## 更新.
+func _physics_process(_delta: float) -> void:
+	match _state:
+		eState.INIT:
+			_state = eState.MAIN
+		eState.MAIN:
+			if Input.is_action_just_released("action"):
+				_state = eState.END
+		eState.END:
+			get_tree().change_scene_to_file("res://src/scenes/title/Title.tscn")
